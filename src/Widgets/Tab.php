358b96418d5b840a52126344520a4ffb298ca387
Arvin Rahnama <arvin.rnm@gmail.com>
2023-11-23 21:15:30 +0330

    update tab components

--- src/Widgets/Tab.php
+++ src/Widgets/Tab.php
@@ -1,187 +1,94 @@
 <?php
+declare(strict_types=1);
 
 namespace Dcat\Admin\Widgets;
 
 use Dcat\Admin\Admin;
 use Illuminate\Contracts\Support\Renderable;
 
-class Tab extends Widget
-{
-    const TYPE_CONTENT = 1;
-    const TYPE_LINK = 2;
+class Tab extends Widget {
+	const TYPE_TAB  = 1;
+	const TYPE_PILL = 2;
 
-    /**
-     * @var string
-     */
-    protected $view = 'admin::widgets.tab';
+	/**
+	 * @var string
+	 */
+	protected $view = 'admin::widgets.tab';
 
-    /**
-     * @var array
-     */
-    protected $data = [
-        'id'       => '',
-        'title'    => '',
-        'tabs'     => [],
-        'dropDown' => [],
-        'active'   => 0,
-        'padding'  => null,
-        'tabStyle' => '',
-    ];
+	/**
+	 * @var array
+	 */
+	protected array $data = [
+		'id' => '',
+		'title' => '',
+		'tabs' => [],
+		'fill' => FALSE,
+	];
 
-    /**
-     * Add a tab and its contents.
-     *
-     * @param  string  $title
-     * @param  string|Renderable  $content
-     * @param  bool  $active
-     * @param  string|null  $id
-     * @return $this
-     */
-    public function add($title, $content, $active = false, $id = null)
-    {
-        $this->data['tabs'][] = [
-            'id'      => $id ?: mt_rand(),
-            'title'   => $title,
-            'content' => $this->toString($this->formatRenderable($content)),
-            'type'    => static::TYPE_CONTENT,
-        ];
+	public function __construct(int $type = self::TYPE_TAB) {
+		$this->data['type'] = $type;
+		return $this;
+	}
 
-        if ($active) {
-            $this->data['active'] = count($this->data['tabs']) - 1;
-        }
+	/**
+	 * Add a tab and its contents.
+	 *
+	 * @param string $title
+	 * @param string|Renderable $content
+	 * @param bool $active
+	 * @param string|null $id
+	 *
+	 * @return $this
+	 */
+	public function add(string $title, string $content, bool $active = FALSE, $icon = FALSE, int $badge = 0, $id = NULL)
+	: Tab {
+		$this->data['tabs'][] = [
+			'id' => $id ?: mt_rand(),
+			'title' => $title,
+			'content' => $this->toString($this->formatRenderable($content)),
+			'active' => $active,
+			'icon' => $icon,
+			'badge' => $badge,
+		];
+		return $this;
+	}
 
-        return $this;
-    }
+	public function fill()
+	: Tab {
+		$this->data['fill'] = TRUE;
+		return $this;
+	}
 
-    /**
-     * Add a link on tab.
-     *
-     * @param  string  $title
-     * @param  string  $href
-     * @param  bool  $active
-     * @return $this
-     */
-    public function addLink($title, $href, $active = false)
-    {
-        $this->data['tabs'][] = [
-            'id'      => mt_rand(),
-            'title'   => $title,
-            'href'    => $href,
-            'type'    => static::TYPE_LINK,
-        ];
+	/**
+	 * Set title.
+	 *
+	 * @param string $title
+	 */
+	public function title($title = '')
+	: Tab {
+		$this->data['title'] = $title;
+		return $this;
+	}
 
-        if ($active) {
-            $this->data['active'] = count($this->data['tabs']) - 1;
-        }
+	/**
+	 * Render Tab.
+	 *
+	 * @return string
+	 */
+	public function render() {
+		$data = array_merge(
+			$this->data,
+			['attributes' => $this->formatHtmlAttributes()]
+		);
+		$this->setupScript();
+		return view($this->view, $data)->render();
+	}
 
-        return $this;
-    }
-
-    /**
-     * Set tab content padding.
-     *
-     * @param  string  $padding
-     */
-    public function padding(string $padding)
-    {
-        $this->data['padding'] = 'padding:'.$padding;
-
-        return $this;
-    }
-
-    public function noPadding()
-    {
-        return $this->padding('0');
-    }
-
-    /**
-     * Set title.
-     *
-     * @param  string  $title
-     */
-    public function title($title = '')
-    {
-        $this->data['title'] = $title;
-
-        return $this;
-    }
-
-    /**
-     * Set drop-down items.
-     *
-     * @param  array  $links
-     * @return $this
-     */
-    public function dropdown(array $links)
-    {
-        if (is_array($links[0])) {
-            foreach ($links as $link) {
-                call_user_func([$this, 'dropDown'], $link);
-            }
-
-            return $this;
-        }
-
-        $this->data['dropDown'][] = [
-            'name' => $links[0],
-            'href' => $links[1],
-        ];
-
-        return $this;
-    }
-
-    public function withCard()
-    {
-        return $this
-            ->class('card', true)
-            ->style('padding:.25rem .4rem .4rem');
-    }
-
-    public function vertical()
-    {
-        return $this
-            ->class('nav-vertical d-block', true)
-            ->style('padding:0!important;')
-            ->tabStyle('nav-left flex-column');
-    }
-
-    public function theme(string $style = 'primary')
-    {
-        return $this
-            ->class('nav-theme-'.$style, true)
-            ->style('padding:0!important;');
-    }
-
-    public function tabStyle($type)
-    {
-        $this->data['tabStyle'] = $type;
-
-        return $this;
-    }
-
-    /**
-     * Render Tab.
-     *
-     * @return string
-     */
-    public function render()
-    {
-        $data = array_merge(
-            $this->data,
-            ['attributes' => $this->formatHtmlAttributes()]
-        );
-
-        $this->setupScript();
-
-        return view($this->view, $data)->render();
-    }
-
-    /**
-     * Setup script.
-     */
-    protected function setupScript()
-    {
-        $script = <<<'SCRIPT'
+	/**
+	 * Setup script.
+	 */
+	protected function setupScript() {
+		$script = <<<'SCRIPT'
 var hash = document.location.hash;
 if (hash) {
     $('.nav-tabs a[href="' + hash + '"]').tab('show');
@@ -192,6 +99,6 @@ $('.nav-tabs a').on('shown.bs.tab', function (e) {
     history.pushState(null,null, e.target.hash);
 });
 SCRIPT;
-        Admin::script($script);
-    }
+		Admin::script($script);
+	}
 }
