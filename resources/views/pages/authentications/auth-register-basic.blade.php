a4cd2d275e9001a92954dc570f836d828f8c63a7
Arvin Rahnama <arvin.rnm@gmail.com>
2023-11-25 03:01:06 +0330

    Update auth-register-basic.blade.php

--- resources/views/pages/authentications/auth-register-basic.blade.php
+++ resources/views/pages/authentications/auth-register-basic.blade.php
@@ -1,92 +1,104 @@
-@php
-$customizerHidden = 'customizer-hide';
-@endphp
+<div class="authentication-wrapper authentication-basic container-p-y">
+  <div class="authentication-inner">
+    <!-- Register -->
+    <div class="card">
+      <div class="card-body">
+        <!-- Logo -->
+        <div class="app-brand justify-content-center">
+          <a href="{{url('/')}}" class="app-brand-link gap-2">
+                    <span class="app-brand-logo">
+                        <img src="{!! config('admin.logo-image') !!}" alt="" class="app-brand-img w-px-150" data-app-light-img="{!! config('admin.logo-image') !!}" data-app-dark-img="{!! config('admin.logo-image-dark') !!}">
+                    </span>
+            <span class="app-brand-text text-body fw-bold">{{config('admin.name')}}</span>
+          </a>
+        </div>
+        <!-- /Logo -->
+        <h4 class="mb-2">{{ __('admin.welcome') }}</h4>
+        <p class="mb-4">{{ __('admin.please_sign_in') }}</p>
 
-@extends('layouts/layoutMaster')
-
-@section('title', 'Register Basic - Pages')
-
-@section('vendor-style')
-<!-- Vendor -->
-<link rel="stylesheet" href="{{asset('assets/vendor/libs/@form-validation/umd/styles/index.min.css')}}" />
-@endsection
-
-@section('page-style')
-<!-- Page -->
-<link rel="stylesheet" href="{{asset('assets/vendor/css/pages/page-auth.css')}}">
-@endsection
-
-@section('vendor-script')
-<script src="{{asset('assets/vendor/libs/@form-validation/umd/bundle/popular.min.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/@form-validation/umd/plugin-bootstrap5/index.min.js')}}"></script>
-<script src="{{asset('assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js')}}"></script>
-@endsection
-
-@section('page-script')
-<script src="{{asset('assets/js/pages-auth.js')}}"></script>
-@endsection
-
-@section('content')
-<div class="container-xxl">
-  <div class="authentication-wrapper authentication-basic container-p-y">
-    <div class="authentication-inner">
-
-      <!-- Register Card -->
-      <div class="card">
-        <div class="card-body">
-          <!-- Logo -->
-          <div class="app-brand justify-content-center">
-            <a href="{{url('/')}}" class="app-brand-link gap-2">
-              <span class="app-brand-logo demo">@include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])</span>
-              <span class="app-brand-text demo text-body fw-bold">{{config('variables.templateName')}}</span>
-            </a>
+        <form id="formAuthentication" class="mb-3" action="{{ admin_route(\Dcat\Admin\Enums\RouteAuth::REGISTER()) }}" method="POST">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+          <div class="mb-3 form-label-group form-group">
+            <label for="name" class="form-label">{{ __('admin.name') }}</label>
+            <input
+                    type="text"
+                    class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
+                    id="name"
+                    name="name"
+                    placeholder="{{ __('admin.name') }}"
+                    autofocus
+                    required
+                    value="{{ old('name') }}"
+            />
+            <div class="help-block with-errors"></div>
           </div>
-          <!-- /Logo -->
-          <h4 class="mb-2">Adventure starts here 🚀</h4>
-          <p class="mb-4">Make your app management easy and fun!</p>
-
-          <form id="formAuthentication" class="mb-3" action="{{url('/')}}" method="GET">
-            <div class="mb-3">
-              <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username" name="username" placeholder="Enter your username" autofocus>
+          <div class="mb-3 form-label-group form-group">
+            <label for="username" class="form-label">{{ __('admin.email_or_username') }}</label>
+            <input
+                    type="text"
+                    class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}"
+                    id="username"
+                    name="username"
+                    placeholder="{{ __('admin.email_or_username') }}"
+                    autofocus
+                    required
+                    value="{{ old('username') }}"
+            />
+            <div class="help-block with-errors"></div>
+          </div>
+          <div class="mb-3 form-password-toggle">
+            <div class="d-flex justify-content-between">
+              <label class="form-label" for="password">{{ trans('admin.password') }}</label>
             </div>
-            <div class="mb-3">
-              <label for="email" class="form-label">Email</label>
-              <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email">
+            <div class="input-group input-group-merge">
+              <input
+                      type="password"
+                      id="password"
+                      class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
+                      name="password"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      required
+                      autocomplete="current-password"
+                      aria-describedby="password"
+              />
+              <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
             </div>
             <div class="mb-3 form-password-toggle">
-              <label class="form-label" for="password">Password</label>
-              <div class="input-group input-group-merge">
-                <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
-                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-              </div>
+            <div class="d-flex justify-content-between">
+              <label class="form-label" for="password">{{ trans('admin.password_confirmation') }}</label>
             </div>
-
-            <div class="mb-3">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms">
-                <label class="form-check-label" for="terms-conditions">
-                  I agree to
-                  <a href="javascript:void(0);">privacy policy & terms</a>
-                </label>
-              </div>
+            <div class="input-group input-group-merge">
+              <input
+                      type="password"
+                      id="password_confirmation"
+                      class="form-control {{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}"
+                      name="password_confirmation"
+                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      required
+                      autocomplete="current-password"
+                      aria-describedby="password"
+              />
+              <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
             </div>
-            <button class="btn btn-primary d-grid w-100">
-              Sign up
-            </button>
-          </form>
+          </div>
+          <div class="mb-3">
+            <button class="btn btn-primary d-grid w-100" type="submit">{{ __('admin.register') }}</button>
+          </div>
+        </form>
 
+        @if(config('admin.auth.allow-register'))
           <p class="text-center">
-            <span>Already have an account?</span>
-            <a href="{{url('auth/login-basic')}}">
-              <span>Sign in instead</span>
+            <span>{{ __('admin.new_on_platform') }}</span>
+            <a href="{{ admin_route(\Dcat\Admin\Enums\RouteAuth::LOGIN()) }}">
+              <span>{{ __('admin.login') }}</span>
             </a>
           </p>
+        @endif
 
+        @if(config('admin.auth.allow-socials'))
           <div class="divider my-4">
-            <div class="divider-text">or</div>
+            <div class="divider-text">{{ __('admin.or') }}</div>
           </div>
-
           <div class="d-flex justify-content-center">
             <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
               <i class="tf-icons bx bxl-facebook"></i>
@@ -100,10 +112,15 @@ $customizerHidden = 'customizer-hide';
               <i class="tf-icons bx bxl-twitter"></i>
             </a>
           </div>
-        </div>
+        @endif
       </div>
-      <!-- Register Card -->
     </div>
   </div>
 </div>
-@endsection
+<script>
+  Dcat.ready(function () {
+    $('#formAuthentication').form({
+      validate: true,
+    });
+  });
+</script>
